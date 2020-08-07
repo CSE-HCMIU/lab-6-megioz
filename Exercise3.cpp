756c4406adf76e787e86a28b63e806b4cbf9a0eb
megioz <69332172+megioz@users.noreply.github.com>
2020-08-07 12:58:40 +0700

    Update Exercise3.cpp

--- Exercise3.cpp
+++ Exercise3.cpp
@@ -10,9 +10,30 @@ Ex:
 #include <stdlib.h>
 
 int ex3(char *str){
-	//your codes here
-}
+	string p;
+	cout << "input a string ";getline(cin, p);
+	char mx = p[0];
+	char mn = p[0];
+	for (int i = 1;i < p.length();i++)
+	{
+		if (p[i] != ' ')
+		{
+			if (p[i] > mx)
+			{
+				mx = p[i];
+			}
+			if (p[i] < mn)
+			{
+				mn = p[i];
+			}
+		}
+	}
+	cout << " max: " << max << "\t\t min: " << min << endl;
+	system("pause");
+	return 0;
 
+	
+}
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	char *str = "This is a string";
