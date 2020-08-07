77ded17413542e4d1f5add9edf74c26e43f525b5
megioz <69332172+megioz@users.noreply.github.com>
2020-08-07 11:01:52 +0700

    Update Exercise1.c

--- Exercise1.c
+++ Exercise1.c
@@ -10,12 +10,149 @@
 #include <stdlib.h>
 
 int ex1(int testcase){
-	//your codes here
+	int n, t, d;
+	n = testcase;
+	t = 1000;
+	while (t){
+		d = n/t;
+		n %= t;
+		if (t > 10 || t == 1 || 10*d >= 20 && 10*d < 10){
+			switch (d){
+				case 1:{
+					printf("one ");
+					break; 
+				}
+				case 2:{
+					printf("two ");
+					break;
+				}
+				case 3:{
+					printf("three ");
+					break;
+				}
+				case 4:{
+					printf("four ");
+					break;
+				}
+				case 5:{
+					printf("five ");
+					break;
+				}
+				case 6:{
+					printf("six ");
+					break;
+				}
+				case 7:{
+					printf("seven ");
+					break;
+				}
+				case 8:{
+					printf("eight ");
+					break;
+				}
+				case 9:{
+					printf("nine ");
+					break;
+				}
+			}
+		}
+		if (t == 1000 && d % t != 0){
+			printf("thousand ");
+		}
+		if (t == 100 && d % t != 0){
+			printf("hundred and ");
+		}
+		if (t == 10){
+			switch (d){
+				case 1:{
+					int k;
+					k = n%t;
+					switch (k){
+						case 1:{
+							printf("and eleven ");
+							break;
+						}
+						case 2:{
+							printf("and twelve ");
+							break;
+						}
+						case 3:{
+							printf("and thirteen ");
+							break;
+						}
+						case 4:{
+							printf("and fourteen ");
+							break;
+						}
+						case 5:{
+							printf("and fifteen ");
+							break;
+						}
+						case 6:{
+							printf("and sixteen ");
+							break;
+						}
+						case 7:{
+							printf("and seventeen ");
+							break;
+						}
+						case 8:{
+							printf("and eighteen ");
+							break;
+						}
+						case 9:{
+							printf("and nineteen ");
+							break;
+						}
+						case 0:{
+							printf(" ten ");
+							break;
+						}
+					}
+					break;
+				}
+				case 2:{
+					printf("twenty ");
+					break;
+				}
+				case 3:{
+					printf("thirty ");
+					break;
+				}
+				case 4:{
+					printf("forty ");
+					break;
+				}
+				case 5:{
+					printf("fifty ");
+					break;
+				}
+				case 6:{
+					printf("sixty ");
+					break;
+				}
+				case 7:{
+					printf("seventy ");
+					break;
+				}
+				case 8:{
+					printf("eighty ");
+					break;
+				}
+				case 9:{
+					printf("ninty ");
+					break;
+				}
+			}
+		}
+		t /= 10;
+	}
 }
 
 int main(int argc, char *argv[]) {
 	int testcase = atoi(argv[1]);
-	
+	printf("input an at most 4 digit number :");
+	scanf("%d",&testcase);
 	ex1(testcase);
 		
 	return 0;
